b55cc68d048df1594592b406d63eb03098e12b60
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-12-13 20:19:26 +0300

    first visualization

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ let minEdgeWeight = 1;
 let maxEdgeWeight = 10
 
 
-function randomKeyFromMap(map) { //! NOT OPTIMIZED AT ALL
-    let target = Math.floor(Math.random() * map.size);
-    let i = 0;
-
-    for (let key of map.keys()) {
-        if (i == target) return key;
-        i++;
-    }
-}
-
-
 function printGraph(graph) {
     for (let i = 0; i < graph.length; i++) {
         if (graph[i].length == 0) {
@@ -26,59 +15,127 @@ function printGraph(graph) {
     }
 }
 
-// Список смежности:
-// двумерный массив: 
-//      список таких списков, в которых эл-ты - 
-//          массив из 2, 
-//              где первое - номер вершины в которую идёт ребро, 
-//              а второе - длина ребра в эту вершину
-
-// Есть много вариантов генерации но я выберу следующий (если не очень, всегда можно переписать)
-
 // TODO: pass all args as one config object
 // TODO: сделать что-то с компонентами связности
-function generateGraph(numberOfVertexes, maxVertexDegree, minEdgeWeight, maxEdgeWeight) {
-    let graph = Array.from({ length: numberOfVertexes }, () => []);
-    
-    let createdEdges = new Map();
-    for (let i = 0; i < numberOfVertexes; i++) {
-        createdEdges.set(i, 0);
+function generateGraph(
+    numberOfVertexes,
+    maxVertexDegree,
+    minEdgeWeight,
+    maxEdgeWeight
+) {
+    const graph = Array.from({ length: numberOfVertexes }, () => []);
+    const degree = Array(numberOfVertexes).fill(0);
+
+    function hasEdge(u, v) {
+        return graph[u].some(([to]) => to === v);
     }
 
-    for (let i = 0; i < numberOfVertexes; i++) {
-        if (graph[i].length > 0) {
-            continue;
+    function addEdge(u, v) {
+        const w = Math.floor(
+        Math.random() * (maxEdgeWeight - minEdgeWeight) + minEdgeWeight
+        );
+
+        graph[u].push([v, w]);
+        graph[v].push([u, w]);
+
+        degree[u]++;
+        degree[v]++;
+    }
+
+    for (let i = 1; i < numberOfVertexes; i++) {
+        let connected = false;
+
+        while (!connected) {
+            const v = Math.floor(Math.random() * i);
+
+            if (
+                degree[i] < maxVertexDegree &&
+                degree[v] < maxVertexDegree &&
+                !hasEdge(i, v)
+            ) {
+                addEdge(i, v);
+                connected = true;
+            }
+        }
+    }
+
+    const maxAttempts = numberOfVertexes * 10;
+    let attempts = 0;
+
+    while (attempts < maxAttempts) {
+        const u = Math.floor(Math.random() * numberOfVertexes);
+        const v = Math.floor(Math.random() * numberOfVertexes);
+
+        if (
+        u !== v &&
+        degree[u] < maxVertexDegree &&
+        degree[v] < maxVertexDegree &&
+        !hasEdge(u, v)
+        ) {
+        addEdge(u, v);
         }
 
-        createdEdges.delete(i);
-
-        for (let j = 0; j < numberOfVertexes; j++) {
-            if (createdEdges.size == 0) {
-                break;
-            }
-
-            let edgeWeight = Math.floor(Math.random() * (maxEdgeWeight - minEdgeWeight) + minEdgeWeight);
-            let destVertex = randomKeyFromMap(createdEdges);
-            
-            graph[i].push([destVertex, edgeWeight]);
-            graph[destVertex].push([i, edgeWeight]);
-
-            let degree = createdEdges.get(destVertex) + 1;
-            createdEdges.set(destVertex, degree);
-            if (degree == maxVertexDegree) {
-                createdEdges.delete(destVertex);
-            }
-        }
-
+        attempts++;
     }
 
     return graph;
 }
 
+
 const graph = generateGraph(
-    10,
+    100,
     4,
     1,
     10
 );
-printGraph(graph);
+
+
+// visualization
+var nodes = new vis.DataSet(
+    Array.from(
+        { length: 100}, (_, i) => {
+            return { 
+                id: i, 
+                label: `${i + 1}`, 
+                shape: "circle",
+                size: 30
+            };
+        }
+    )
+)
+
+let addedEdges = new Set();
+let edgeObjs = new Array();
+for (let i = 0; i < graph.length; i++) {
+    let edges = graph[i];
+    for (let edge of edges) {
+        if (addedEdges.has(`${edge[0]}-${i}`)) 
+            continue;
+        edgeObjs.push({
+            from: i,
+            to: edge[0],
+            label: `${edge[1]}`,
+            arrows: ''
+        })
+        addedEdges.add(`${i}-${edge[0]}`);
+    }
+}
+let edges = new vis.DataSet(edgeObjs); 
+
+var container = document.getElementById("network");
+var data = {
+    nodes: nodes,
+    edges: edges
+}
+var options = {
+    physics: {
+        enabled: true,
+        barnesHut: {
+        springLength: 150,
+        },
+    }
+};
+
+var network = new vis.Network(container, data, options);
+
+alert("where is my mind?");
